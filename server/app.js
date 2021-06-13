071366bf449b029a167be250e189a408ff0f50e3
MacBook Pro <macbookpro@Ghuns-MacBook-Pro.local>
2021-06-13 21:52:51 +0700

    First commit

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const url = 'mongodb://127.0.0.1:27017/graphql-server'
+const db = mongoose.connection;
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+db.once('open', _ => {
+    console.log('Database connected:', url);
+});
+db.once('error', err => {
+    console.error('connection error:', err);
+});
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+app.listen(4000, () => console.log("Listening on port 4000..."))
